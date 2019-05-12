9566517712c17330bea2d316e3b7a2046e62a91d
ProV1denCEX <firstematxcx@gmail.com>
2019-05-12 15:43:47 -0400

    2nd method for tricks

--- Tricks/MATLAB/get_yyyymmdd.m
+++ Tricks/MATLAB/get_yyyymmdd.m
@@ -3,6 +3,15 @@
 % 38s
 tic
 for i = 1 : 5000
-    aaa = num2cell(str2double(datestr(cDate, 'yyyymmdd')))
+    aaa = num2cell(str2double(datestr(cDate, 'yyyymmdd')));
+end
+toc
+
+% 7s
+tic
+for i = 1 : 5000
+    cTemp = regexp(cDate, '\d', 'match');
+    cTemp = num2cell(str2double(cellfun(@(x) [x{:}], cTemp, 'UniformOutput', false)));
+    cTemp = cTemp';
 end
 toc
